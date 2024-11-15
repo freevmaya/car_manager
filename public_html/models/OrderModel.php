8caf279139fb69d46de0783a4c0d537d29896366
Vadim Frolov <@vmaya>
2024-11-15 20:27:07 +0500

    requlary

--- public_html/models/OrderModel.php
+++ public_html/models/OrderModel.php
@@ -39,7 +39,7 @@ class OrderModel extends BaseModel {
 			"LEFT JOIN car c ON c.id=driverOnTheLine.car_id ".
 			"LEFT JOIN car_bodies cb ON cb.id=c.car_body_id ".
 			"LEFT JOIN car_color cc ON cc.id=c.color_id ".
-			"WHERE $whereStr ORDER BY `id` DESC ASD";
+			"WHERE $whereStr ORDER BY `id` DESC";
 
 		if (isset($options['limit']))
 			$query .= " LIMIT 0, {$options['limit']}";
