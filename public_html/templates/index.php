fe36962cedf06cea13ed9d4d194b4949ebfe4aba
Vadim Frolov <@vmaya>
2025-02-10 14:06:42 +0500

    requlary

--- public_html/templates/index.php
+++ public_html/templates/index.php
@@ -2,7 +2,7 @@
 GLOBAL $devUser, $user, $lang, $anti_cache;
 include_once(MODULESPATH.'/views.php');
 
-$anti_cache = '?=45';
+$anti_cache = '?=46';
 
 $options = ['user_id' => $user['id'], 'state'=>['receive', 'active']];
 html::AddJsData(json_encode(
